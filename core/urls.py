e09a36c292dfabacffa8f6b2761ca0afb02a79ef
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-03-15 20:16:09 +1100

    fix syntax

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from .views import stats_regions
 urlpatterns = [
     path('hello/', views.hello_world),
     path('search-def/', views.simple_scrapy_test, name='search_def'),
-    path('stats/regions', stats_regions, name="stats-regions"),
-    path('stats/diseases', views.stats_diseases, name="stats-diseases")
+    path('stats/regions', stats_regions, name="stats_regions"),
+    path('stats/diseases', views.stats_diseases, name="stats_diseases")
 ]
 
