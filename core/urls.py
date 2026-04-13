241e1c1b659434808a2aaa554684056ac030bfc8
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-04-13 18:20:40 +1000

    move authentication into another app

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import get_alerts, stats_regions, stats_timeseries
-from .views import RegisterView, LoginView
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -10,6 +9,4 @@ urlpatterns = [
     path("stats/regions/", stats_regions, name="stats_regions"),
     path("stats/diseases/", views.stats_diseases, name="stats_diseases"),
     path("summary/region/", views.region_summary_view, name="region_summary"),
-    path("auth/signup1/", RegisterView.as_view(), name="signup"),
-    path("auth/signin1/", LoginView.as_view(), name="signin"),
 ]
--- core/views.py
+++ core/views.py
@@ -12,52 +12,21 @@ from drf_yasg import openapi
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, generics
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
+from rest_framework import status
 
 from core.models import Alert
-from .serializers import RegisterSerializer
-# UserSerializer imported only if needed elsewhere
 
-# ------------------------
-# Signup
-# ------------------------
-
-
-class RegisterView(generics.CreateAPIView):
-    serializer_class = RegisterSerializer
-    permission_classes = [AllowAny]  # public access
-
-
-# ------------------------
-# Signin
-# ------------------------
-class LoginView(APIView):
-    permission_classes = [AllowAny]  # public access
-
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key})
-        return Response(
-            {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
-        )
-
-
-# ------------------------
-# Swagger query parameters
-# ------------------------
 common_filter_parameters = [
     openapi.Parameter(
         "id",
         openapi.IN_QUERY,
         description="Filter by external alert identifier",
+        type=openapi.TYPE_STRING,
+    ),
+    openapi.Parameter(
+        "from",
+        openapi.IN_QUERY,
+        description="Start date in YYYY-MM-DD format",
         type=openapi.TYPE_STRING,
     ),
     openapi.Parameter(
